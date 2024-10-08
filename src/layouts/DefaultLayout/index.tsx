42201c33a029732e31a78ca72b31736214ec31eb
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-10-08 14:16:03 -0300

    css: definiando css do layout das paginas

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../components/Header/index,";
+import { LayoutContainer } from "./style";
 
 export function DefaultLayout(){
     return (
-        <main> 
+        <LayoutContainer> 
             <Header />
             <Outlet />
-        </main>
+        </LayoutContainer>
     );
 }
