8431ec2530158802e9d550061e3ba378ae77b811
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-10-08 22:08:58 -0300

    reafactor: colocando profile dentro do header

--- src/components/Header/index,.tsx
+++ src/components/Header/index,.tsx
@@ -1,11 +1,51 @@
 import logo from '../../assets/logo.svg'
 import { HeaderContainer } from './styles';
 
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Profile, ProfileHeader, ProfileInfo } from "./styles";
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { faBuilding } from "@fortawesome/free-regular-svg-icons/faBuilding";
+import { faUserGroup } from "@fortawesome/free-solid-svg-icons/faUserGroup";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons/faArrowUpRightFromSquare";
+
+
 export function Header(){
     return (
         <HeaderContainer>
             <img src={logo} alt="" />
             <h1>GITHUB BLOG</h1>
+
+            <Profile>
+                <img src="https://github.com/lwkas-gabriel.png" alt="" />
+                <ProfileInfo>
+                    <ProfileHeader>
+                        <h3>Lucas Gabriel Correia Barros</h3>
+                        <a href="">GITHUB <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
+                    </ProfileHeader>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis soluta sint id assumenda possimus, veniam totam enim aspernatur praesentium minima!</p>
+                    <ul>
+                        <li>
+                            <label htmlFor="">
+                                <FontAwesomeIcon icon={faGithub} />
+                            </label>
+                            lwkas-gabriel
+                        </li>
+                        <li>
+                            <label htmlFor="">
+                                <FontAwesomeIcon icon={faBuilding} />
+                            </label>
+                            Rocketseat
+                        </li>
+                        <li>
+                            <label htmlFor="">
+                                <FontAwesomeIcon icon={faUserGroup} />
+                            </label>
+                            51 seguidores
+                        </li>
+                    </ul>
+                </ProfileInfo>
+            </Profile>
         </HeaderContainer>
     );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -24,3 +24,76 @@ export const HeaderContainer = styled.header`
         color: ${props => props.theme.blue};
     }
 `
+
+export const Profile = styled.section`
+    height: 230px;
+    display: flex;
+    align-items: center;
+    padding: 0 32px;
+    gap: 2rem;
+    background-color: ${props => props.theme["base-post"]};
+    border-radius: 10px;
+    padding: 20px;
+    margin-top: 50px;
+
+    max-width: 868px;
+    
+    img{
+        width: 148px;
+        height: 148px;
+        border-radius: 8px;
+    }
+`;
+
+export const ProfileHeader = styled.div`
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 10px;
+        color: ${props => props.theme["base-title"]};
+
+
+        h3{
+            font-size: 24px;
+            font-weight: bold;
+            line-height: 130%;
+        }
+
+        a{ 
+            display: flex;
+            align-items: center;
+            font-size: 12px;
+            font-weight: bold;
+            color: ${props => props.theme.blue};
+            gap: 5px;
+            text-decoration: none;
+            cursor: pointer;
+        }
+`;
+
+export const ProfileInfo = styled.div`
+        display: flex;
+        flex-direction: column;
+
+        p{
+            color: ${props=> props.theme["base-text"]};
+            margin-bottom: 24px;
+        }
+
+        ul{
+            display: flex;
+            gap: 24px;
+            list-style: none;
+
+            li{
+                display: flex;
+                align-items: center;
+                gap: .5rem;
+                color: ${props => props.theme["base-subtitle"]};
+
+                label{
+                    color: ${props => props.theme["base-labal"]};
+                }
+            }
+        }
+`;
