e47dddd8f4bec74ba4fb8e53ed20ada4767cc913
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-10-08 22:09:20 -0300

    feat: criando e estilizando o card de Post

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,44 +1,73 @@
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { BlogContainer, Profile, ProfileHeader, ProfileInfo } from "./styles";
-import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
-import { faBuilding } from "@fortawesome/free-regular-svg-icons/faBuilding";
-import { faUserGroup } from "@fortawesome/free-solid-svg-icons/faUserGroup";
-import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons/faArrowUpRightFromSquare";
-
+import { BlogContainer, Post, PostHeader, PostList,SearchForm } from "./styles";
 export function Blog(){
     return (
         <BlogContainer>
-            <Profile>
-                <img src="https://github.com/lwkas-gabriel.png" alt="" />
-                <ProfileInfo>
-                    <ProfileHeader>
-                        <h3>Lucas Gabriel Correia Barros</h3>
-                        <a href="">GITHUB <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
-                    </ProfileHeader>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis soluta sint id assumenda possimus, veniam totam enim aspernatur praesentium minima!</p>
-                    <ul>
-                        <li>
-                            <label htmlFor="">
-                                <FontAwesomeIcon icon={faGithub} />
-                            </label>
-                            lwkas-gabriel
-                        </li>
-                        <li>
-                            <label htmlFor="">
-                                <FontAwesomeIcon icon={faBuilding} />
-                            </label>
-                            Rocketseat
-                        </li>
-                        <li>
-                            <label htmlFor="">
-                                <FontAwesomeIcon icon={faUserGroup} />
-                            </label>
-                            51 seguidores
-                        </li>
-                    </ul>
-                </ProfileInfo>
-            </Profile>
+            <SearchForm action="">
+                <div>
+                    <h4>Publicações</h4>
+                    <label htmlFor="">6 Publicações</label>
+                </div>
+                <input type="text" placeholder="Buscar Conteúdo" />
+            </SearchForm>
+
+            <PostList>
+                <Post>
+                    <PostHeader>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <span>Há 1 dia</span>
+                    </PostHeader>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam id mollitia eveniet magnam voluptatibus iste dignissimos tenetur iure tempore quo? Commodi, perspiciatis eum quos sequi aut minima odit! Voluptatum, ut!</p>
+                </Post>
+
+                <Post>
+                    <PostHeader>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <span>Há 1 dia</span>
+                    </PostHeader>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam id mollitia eveniet magnam voluptatibus iste dignissimos tenetur iure tempore quo? Commodi, perspiciatis eum quos sequi aut minima odit! Voluptatum, ut!</p>
+                </Post>
+
+                <Post>
+                    <PostHeader>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <span>Há 1 dia</span>
+                    </PostHeader>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam id mollitia eveniet magnam voluptatibus iste dignissimos tenetur iure tempore quo? Commodi, perspiciatis eum quos sequi aut minima odit! Voluptatum, ut!</p>
+                </Post>
+
+                <Post>
+                    <PostHeader>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <span>Há 1 dia</span>
+                    </PostHeader>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam id mollitia eveniet magnam voluptatibus iste dignissimos tenetur iure tempore quo? Commodi, perspiciatis eum quos sequi aut minima odit! Voluptatum, ut!</p>
+                </Post>
+
+                <Post>
+                    <PostHeader>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <span>Há 1 dia</span>
+                    </PostHeader>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam id mollitia eveniet magnam voluptatibus iste dignissimos tenetur iure tempore quo? Commodi, perspiciatis eum quos sequi aut minima odit! Voluptatum, ut!</p>
+                </Post>
+
+                <Post>
+                    <PostHeader>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <span>Há 1 dia</span>
+                    </PostHeader>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam id mollitia eveniet magnam voluptatibus iste dignissimos tenetur iure tempore quo? Commodi, perspiciatis eum quos sequi aut minima odit! Voluptatum, ut!</p>
+                </Post>
+
+                <Post>
+                    <PostHeader>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <span>Há 1 dia</span>
+                    </PostHeader>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam id mollitia eveniet magnam voluptatibus iste dignissimos tenetur iure tempore quo? Commodi, perspiciatis eum quos sequi aut minima odit! Voluptatum, ut!</p>
+                </Post>
+            </PostList>
 
         </BlogContainer>
     );
--- src/pages/Blog/styles.ts
+++ src/pages/Blog/styles.ts
@@ -1,77 +1,88 @@
 import styled from "styled-components";
 
 export const BlogContainer = styled.main`
-    margin-top: -90px;
+    margin-top: 100px;
     display: flex;
-    justify-content: center;
-`
-
-export const Profile = styled.section`
-    width: 70%;
-    height: 230px;
-    display: flex;
+    flex-direction: column;
     align-items: center;
-    padding: 0 32px;
-    gap: 2rem;
-    background-color: ${props => props.theme["base-post"]};
-    border-radius: 10px;
-    
-    img{
-        width: 148px;
-        height: 148px;
-        border-radius: 8px;
-    }
-`
-
-export const ProfileHeader = styled.div`
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        margin-bottom: 10px;
-        color: ${props => props.theme["base-title"]};
-
-
-        h3{
-            font-size: 24px;
-            font-weight: bold;
-            line-height: 130%;
-        }
-
-        a{ 
-            display: flex;
-            align-items: center;
-            font-size: 12px;
-            font-weight: bold;
-            color: ${props => props.theme.blue};
-            gap: 5px;
-            text-decoration: none;
-            cursor: pointer;
-        }
+    justify-content: center;
+    max-width: 868px;
+    width: 100%;
 `;
 
-export const ProfileInfo = styled.div`
+export const SearchForm = styled.form`
+    width: inherit;
+    margin-top: 72px;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+
+    div{
         display: flex;
-        flex-direction: column;
+        justify-content: space-between;
 
-        p{
-            color: ${props=> props.theme["base-text"]};
-            margin-bottom: 24px;
+        h4{
+            color: ${props => props.theme["base-subtitle"]};
+            font-size: 18px;
         }
 
-        ul{
-            display: flex;
-            gap: 24px;
-            list-style: none;
-
-            li{
-                display: flex;
-                align-items: center;
-                gap: .5rem;
-                color: ${props => props.theme["base-subtitle"]};
-
-                label{
-                    color: ${props => props.theme["base-labal"]};
-                }
-            }
+        label{
+            color: ${props => props.theme["base-span"]};
+            font-size: 14px;
         }
-`
+    }
+
+    input{
+        width: 100%;
+        padding: 12px 16px;
+        border-radius: 6px;
+        background-color: ${props => props.theme["base-input"]};
+        border: 1px solid ${props => props.theme["base-border"]};
+        color: ${props => props.theme["base-text"]};
+
+        &::placeholder{
+            color: ${props => props.theme["base-labal"]};
+        }
+    }
+`;
+
+export const PostList = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 48px;
+    gap: 1rem;
+    margin-bottom: 246px;
+`;
+
+export const Post = styled.div`
+    width: 416px;
+    height: 260px;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    border-radius: 10px;
+    padding: 32px;
+
+    background-color: ${props => props.theme["base-post"]};
+
+    p{
+        color: ${props => props.theme["base-text"]};
+    }
+`;
+
+export const PostHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    h3{
+        font-size: 20px;
+        color: ${props => props.theme["base-title"]}
+    }
+
+    span{
+        font-size: 14px;
+        color: ${props => props.theme["base-span"]}
+    }
+`;
